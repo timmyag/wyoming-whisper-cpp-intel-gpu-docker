b00fce439afa4169f7c113d68cd089c827c602a7
tannisroot <10602045+tannisroot@users.noreply.github.com>
2025-02-22 08:21:38 +0300

    Improve run script

--- run.sh
+++ run.sh
@@ -2,12 +2,28 @@
 
 set -o pipefail
 
+if [ -z "$BEAM_SIZE" ]; then
+    BEAM_SIZE=5
+fi
+echo "Beam size set to $BEAM_SIZE"
+
+if [ -z "$MODEL" ]; then
+    echo "Model name not set" >&2
+    exit 1
+fi
 MODEL_FILE="/models/ggml-$MODEL.bin"
 
-if [ ! -f "$MODEL_FILE" ]; then
+if [ ! -f "/models/download-ggml-model.sh" ]; then
     curl https://raw.githubusercontent.com/ggerganov/whisper.cpp/refs/heads/master/models/download-ggml-model.sh > /models/download-ggml-model.sh && chmod +x /models/download-ggml-model.sh
-    cd /models && ./download-ggml-model.sh ${MODEL}
 fi
 
-( cd /data/whisper.cpp && build/bin/whisper-server -t 1 -bs 5 -m "$MODEL_FILE" --host 127.0.0.1 --port 8910 --suppress-nst ) &
+if [ ! -f "$MODEL_FILE" ]; then
+    cd /models || { echo "Could not open /models"; exit 1; }
+    if ! ./download-ggml-model.sh ${MODEL}; then
+        echo "Failed to download $MODEL model" >&2
+        exit 1
+    fi
+fi
+
+( cd /data/whisper.cpp && build/bin/whisper-server -bs ${BEAM_SIZE} -m ${MODEL_FILE} --host 127.0.0.1 --port 8910 --suppress-nst ${EXTRA_ARGS} ) &
 ( cd /data/wyoming-whisper-api-client && script/run --uri tcp://0.0.0.0:7891 --api http://127.0.0.1:8910/inference )
